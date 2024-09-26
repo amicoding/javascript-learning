687fc88e4102c2016109028928bc3141ddeade01
amicoding <mdsagarkhan16656@gmail.com>
2024-09-26 23:15:06 +0600

    3.js file updated

--- 3.js
+++ 3.js
@@ -1,2 +1,38 @@
-var sum = 30;
-console.log(sum)
+
+function adds(a, b){
+  
+  var sum = a+b ;
+  return sum
+}
+
+
+function subs(c, d){
+  
+  var sub = c-d ;
+  return sub
+}
+
+
+function muls(e, f){
+  
+  var mul = e*f ;
+  return mul
+}
+
+
+function divs(g, h){
+  
+  var div = g/h;
+  return div
+}
+
+
+ var sums = adds(200,5);
+
+ var sub = subs(100,2);
+
+var mul = muls(100,7);
+
+var div = divs(100,4);
+
+console.log("Summation:" ,sums,"," ,"Substraction:",sub ,"Multiple:", mul, "Divide:", div);
