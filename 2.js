24f29b847403d309f83b55fae88f1c041a776e1d
amicoding <mdsagarkhan16656@gmail.com>
2024-09-27 15:50:35 +0600

    function for loop array

---
+++ 2.js
@@ -0,0 +1,8 @@
+
+var names = [
+  "sagar","rayhan","sakib","raju"
+  ];
+  
+  for (var i = 0; i < names.length; i++) {
+    console.log(names[i])
+  }
---
+++ 4.js
@@ -0,0 +1,37 @@
+
+
+var names = [
+  "sagar","rayhan","sakib","raju"
+  ,
+  {
+    
+    name: "Md Sagar Khan",
+    roll: 03,
+    dept: "Math"
+  }
+  
+  ];
+  
+  var num= [
+    55, 765, 765
+    ]
+  /*
+  for (var i = 0; i < names.length; i++) {
+    console.log(names[i])
+  }*/
+  
+  
+  function allName(pm1){
+    for (let i = 0; i < pm1.length; i++) {
+      
+      console.log(pm1[i]);
+      
+    }
+  }
+  
+  
+  allName(num);
+  allName(names);
+  
+  
+  
