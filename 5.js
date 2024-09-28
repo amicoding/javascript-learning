6ddae13e60b1b15ad1277c3ca4ed6c4af338cfbb
amicoding <mdsagarkhan16656@gmail.com>
2024-09-28 21:48:42 +0600

    Array and Object Combination

---
+++ 5.js
@@ -0,0 +1,23 @@
+
+
+var mobile = [
+  "itel","samsung","techno","symphony", {
+    name: "itel",
+    price: "12200",
+    color: "black",
+    samsung: ["price:",20000, "color","gold"],
+    techno: ["price:",15000, "color","blue",]
+  }
+  ]
+  
+  /*console.log(mobile)*/
+  
+  var details ={
+    name: "itel",
+    price: "12200",
+    color: "black",
+    samsung: ["price:",20000, "color","gold"],
+    techno: ["price:",15000, "color","blue",]
+  }
+  
+  console.log(mobile)
