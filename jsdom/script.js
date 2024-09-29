2892e3f149357918ecfcf90a0a9f0144ee6977ce
amicoding <mdsagarkhan16656@gmail.com>
2024-09-30 03:05:09 +0600

    selector, target tag by id, class

--- jsdom/script.js
+++ jsdom/script.js
@@ -1,2 +1,31 @@
 
+
+
+
 // console.log("hi")
+
+// terget by class name 
+
+ var headings=document.getElementsByClassName('heading')
+ var changes= headings[2];
+var changess= changes.innerText= "i am changed"
+ 
+console.log(changess)
+
+var heading= document.getElementById('head');
+
+var change= heading;
+change.innerText = "i have changed"
+
+
+
+
+/*var heading= document.querySelector("#head")
+
+
+console.log(heading)
+*/
+
+
+
+
