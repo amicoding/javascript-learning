4719c84c7af0e8c28b0a87336197e106166d2e67
amicoding <mdsagarkhan16656@gmail.com>
2024-10-01 23:30:56 +0600

    onclick event , change style of element from js file

---
+++ 8.js
@@ -0,0 +1,57 @@
+
+
+let head = document.getElementById('head');
+
+let input = document.getElementById('input');
+
+let btn = document.getElementById('btn');
+
+let para = document.getElementById('para');
+
+// console.log();
+ head.style.backgroundColor = 'green' ;
+ head.style.color = 'blue' ;
+ 
+ function clickFun(){
+   alert("hello world");
+ }
+ 
+ function paraFun(){
+   let p = document.getElementById('para');
+   p.style.backgroundColor = "green";
+   p.style.color = "white";
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
