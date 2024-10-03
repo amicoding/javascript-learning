3e302148edce9b37ba0c6301156505598813b278
amicoding <mdsagarkhan16656@gmail.com>
2024-10-03 17:49:38 +0600

    home-work folder created and script.js file created

---
+++ Home-work/script.js
@@ -0,0 +1,80 @@
+
+// problem 1 start
+
+let appleQt = 25;
+let buyApple = 15;
+
+let totalApple = appleQt + buyApple;
+
+// console.log("Sarah now has the apple : " + totalApple  );
+// problem 1 end
+
+
+// problem 2 start
+
+    let totalBalance = 500;
+    let cost = 200;
+    let remainBalance = totalBalance - cost;
+    
+    console.log("Remain Balance of John : "+ remainBalance);
+
+// problem 2 end
+
+//problem 3 start
+
+  let pRateteHour = 120;
+  totalHour = 8;
+  totalProduction = 120*8;
+  console.log("Total Production within 8 hour: "+ totalProduction);
+//problem 3 end
+
+// problem 4 start
+
+    let treeInRow = 8;
+    let totalRow = 12;
+    let willPlant = 20;
+    let totalTree = treeInRow * totalRow + willPlant;
+    console.log("Now available tree in the garden: "+ totalTree);
+// problem 4 end
+
+// problem 5 start
+
+   let totalBox = 5;
+   let orangePerBox = 10;
+   let giveOrange = 12;
+   availableOrange = totalBox * orangePerBox - giveOrange;
+   
+   console.log("Now available Orange to Rahim : " + availableOrange);
+
+// problem 5 end
+
+// problem 6 start
+
+
+  let totalRoad = 120;
+  let totalTime = 3;
+  let speedPerHour = 120/3;
+  
+  console.log("Train will pass in 1 hour: "+ speedPerHour);
+
+// problem 7 end
+
+// problem 8 start
+
+
+// problem 8 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
